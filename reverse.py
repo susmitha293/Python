1c956bc42f01317225b5f5a3f260771fc5577eb4
Susmitha Reddy Banda <Susmitha.Reddy.Banda@ibm.com>
2026-02-28 12:38:10 +0530

    modified code

--- reverse.py
+++ reverse.py
@@ -1,9 +1,25 @@
+# def reverse_num(n):
+#     rev=0
+#     while(n>0):
+#         d=n%10
+#         rev=rev*10+d
+#         n=n//10
+#     return rev
+# num=int(input())
+# print(reverse_num(num))
+
+
 def reverse_num(n):
+    if(n<0):
+        sign=-1
+    else:
+        sign=1
+    n = abs(n) 
     rev=0
     while(n>0):
         d=n%10
         rev=rev*10+d
         n=n//10
-    return rev
+    return rev*sign
 num=int(input())
 print(reverse_num(num))
